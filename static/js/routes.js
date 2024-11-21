f62b83d353ce29f4fdfd11bba021fa4510bfd83c
rndlrndl <rndl28@naver.com>
2024-11-21 16:35:29 +0900

    routing

--- static/js/routes.js
+++ static/js/routes.js
@@ -13,6 +13,46 @@ module.exports = function(app, session, isAuthenticated)
         res.render('template/login.html');
     })
 
+    app.get('/projects', function(req, res){
+        res.render('template/projects.html');
+    })
+
+    app.get('/view_project', function(req, res){
+        res.render('template/view_project.html');
+    })
+
+    app.get('/projects_plus', function(req, res){
+        res.render('template/projects_plus.html');
+    })
+
+    app.get('/create_project', function(req, res){
+        res.render('template/create_project.html');
+    })
+
+    app.get('/create_discussion', function(req, res){
+        res.render('template/create_discussion.html');
+    })
+
+    app.get('/discussions', function(req, res){
+        res.render('template/discussions.html');
+    })
+
+    app.get('/discussions_plus', function(req, res){
+        res.render('template/discussions_plus.html');
+    })
+
+    app.get('/view_discussion', function(req, res){
+        res.render('template/view_discussion.html');
+    })
+
+    app.get('/index', function(req, res){
+      res.render('template/index.html');
+  })
+
+    app.get('/search', function(req, res){
+      res.render('template/search.html');
+    })
+
     app.get('/mypage', isAuthenticated, (req, res) => {
       const userinfo = req.session.user;
       res.render('template/mypage.html', {userinfo});
